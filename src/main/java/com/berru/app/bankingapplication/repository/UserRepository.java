7a4ae38e4dded944c2b7d18f28c172316794ab70
Berru Hanedar <berruhanedar@icloud.com>
2025-09-04 03:33:49 +0300

    add existsByEmail method to UserRepository

--- src/main/java/com/berru/app/bankingapplication/repository/UserRepository.java
+++ src/main/java/com/berru/app/bankingapplication/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.berru.app.bankingapplication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
 }
