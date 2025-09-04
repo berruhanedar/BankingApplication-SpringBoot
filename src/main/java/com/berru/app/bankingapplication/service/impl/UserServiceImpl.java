7d90420b2da3ea1701ddf32e3ce90bdd53bc4d2c
Berru Hanedar <berruhanedar@icloud.com>
2025-09-04 03:36:07 +0300

    implement UserServiceImpl with account creation logic

--- src/main/java/com/berru/app/bankingapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/berru/app/bankingapplication/service/impl/UserServiceImpl.java
@@ -2,11 +2,37 @@ package com.berru.app.bankingapplication.service.impl;
 
 import com.berru.app.bankingapplication.dto.BankResponse;
 import com.berru.app.bankingapplication.dto.CreateUserRequestDTO;
+import com.berru.app.bankingapplication.entity.User;
+import com.berru.app.bankingapplication.exception.BadRequestException;
+import com.berru.app.bankingapplication.mapper.UserMapper;
+import com.berru.app.bankingapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserMapper userMapper, UserRepository userRepository) {
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+    }
 
     @Override
     public BankResponse createAccount(CreateUserRequestDTO createUserRequestDTO) {
-        return null;
+
+        if (userRepository.existsByEmail(createUserRequestDTO.getEmail())) {
+            throw new BadRequestException("This user already has an account created!");
+        }
+
+        User newUser = userMapper.toEntity(createUserRequestDTO);
+
+        User savedUser = userRepository.save(newUser);
+
+        return userMapper.toBankResponse(savedUser);
+
     }
 }
