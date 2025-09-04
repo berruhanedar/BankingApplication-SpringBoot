2c5a074986a05bc0be8e0c03de690310711fbeda
Berru Hanedar <berruhanedar@icloud.com>
2025-09-04 03:26:35 +0300

    remove responseCode from BankResponse DTO

--- src/main/java/com/berru/app/bankingapplication/dto/BankResponse.java
+++ src/main/java/com/berru/app/bankingapplication/dto/BankResponse.java
@@ -11,8 +11,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BankResponse {
 
-    private String responseCode;
-
     private String responseMessage;
 
     private AccountInfo accountInfo;
