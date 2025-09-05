2c0c2864c2b4c7500ecd0e8380f4358428639291
Berru Hanedar <berruhanedar@icloud.com>
2025-09-05 04:07:06 +0300

    create EmailDetails DTO

---
+++ src/main/java/com/berru/app/bankingapplication/dto/EmailDetails.java
@@ -0,0 +1,23 @@
+package com.berru.app.bankingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailDetails {
+
+    private String recipient;
+
+    private String messageBody;
+
+    private String subject;
+
+    private String attachment;
+
+}
+
