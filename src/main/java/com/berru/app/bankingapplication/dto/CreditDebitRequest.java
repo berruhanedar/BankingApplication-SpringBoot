0836e91a075a724e01fde4d611bda4441fa07231
Berru Hanedar <berruhanedar@icloud.com>
2025-09-07 03:40:50 +0300

    add CreditDebitRequest DTO for account transactions

---
+++ src/main/java/com/berru/app/bankingapplication/dto/CreditDebitRequest.java
@@ -0,0 +1,17 @@
+package com.berru.app.bankingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditDebitRequest {
+
+    private String accountNumber;
+
+    private BigDecimal amount;
+}
