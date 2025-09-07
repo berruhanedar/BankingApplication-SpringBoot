2f6a53a240bbeae30bd29e9ee11f9b329a7405b7
Berru Hanedar <berruhanedar@icloud.com>
2025-09-07 03:43:20 +0300

    add creditAccount method to UserService

--- src/main/java/com/berru/app/bankingapplication/service/impl/UserService.java
+++ src/main/java/com/berru/app/bankingapplication/service/impl/UserService.java
@@ -2,9 +2,11 @@ package com.berru.app.bankingapplication.service.impl;
 
 import com.berru.app.bankingapplication.dto.BankResponse;
 import com.berru.app.bankingapplication.dto.CreateUserRequestDTO;
+import com.berru.app.bankingapplication.dto.CreditDebitRequest;
 
 public interface UserService {
     BankResponse createAccount(CreateUserRequestDTO createUserRequestDTO);
     BankResponse balanceEnquiry(String accountNumber);
     String nameEnquiry(String accountNumber);
+    BankResponse creditAccount(CreditDebitRequest  creditDebitRequest);
 }
