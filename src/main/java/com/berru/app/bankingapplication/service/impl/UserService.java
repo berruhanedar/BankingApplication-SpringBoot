d0859ce6e27c192f8f8f17540821ee86c400e371
Berru Hanedar <berruhanedar@icloud.com>
2025-09-09 17:30:35 +0300

    add transfer method to UserService interface

--- src/main/java/com/berru/app/bankingapplication/service/impl/UserService.java
+++ src/main/java/com/berru/app/bankingapplication/service/impl/UserService.java
@@ -3,6 +3,7 @@ package com.berru.app.bankingapplication.service.impl;
 import com.berru.app.bankingapplication.dto.BankResponse;
 import com.berru.app.bankingapplication.dto.CreateUserRequestDTO;
 import com.berru.app.bankingapplication.dto.CreditDebitRequest;
+import com.berru.app.bankingapplication.dto.TransferInfo;
 
 public interface UserService {
     BankResponse createAccount(CreateUserRequestDTO createUserRequestDTO);
@@ -10,4 +11,5 @@ public interface UserService {
     String nameEnquiry(String accountNumber);
     BankResponse creditAccount(CreditDebitRequest  creditDebitRequest);
     BankResponse debitAccount(CreditDebitRequest creditDebitRequest);
+    BankResponse transfer(TransferInfo transferInfo);
 }
