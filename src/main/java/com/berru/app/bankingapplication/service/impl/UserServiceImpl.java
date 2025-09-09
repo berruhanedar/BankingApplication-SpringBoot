ea4a6d755e82ef2a3e9cf6745d8b808f7ff7f411
Berru Hanedar <berruhanedar@icloud.com>
2025-09-09 17:33:02 +0300

    refactor UserServiceImpl and add transfer functionality

--- src/main/java/com/berru/app/bankingapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/berru/app/bankingapplication/service/impl/UserServiceImpl.java
@@ -3,10 +3,13 @@ package com.berru.app.bankingapplication.service.impl;
 import com.berru.app.bankingapplication.dto.*;
 import com.berru.app.bankingapplication.entity.User;
 import com.berru.app.bankingapplication.exception.BadRequestException;
+import com.berru.app.bankingapplication.exception.DestinationAccountNotFoundException;
 import com.berru.app.bankingapplication.exception.InsufficientBalanceException;
+import com.berru.app.bankingapplication.exception.SourceAccountNotFoundException;
 import com.berru.app.bankingapplication.mapper.EmailMapper;
 import com.berru.app.bankingapplication.mapper.UserMapper;
 import com.berru.app.bankingapplication.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,56 +32,44 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public BankResponse createAccount(CreateUserRequestDTO createUserRequestDTO) {
-
         if (userRepository.existsByEmail(createUserRequestDTO.getEmail())) {
             throw new BadRequestException("This user already has an account created!");
         }
 
         User newUser = userMapper.toEntity(createUserRequestDTO);
-
         User savedUser = userRepository.save(newUser);
 
         EmailDetails emailDetails = emailMapper.toEmailDetails(savedUser);
-
         emailService.sendEmailAlert(emailDetails);
 
         return userMapper.toBankResponse(savedUser);
-
     }
 
     @Override
     public BankResponse balanceEnquiry(String accountNumber) {
-        boolean isAccountExist = userRepository.existsByAccountNumber(accountNumber);
-        if (!isAccountExist) {
-            throw new BadRequestException("This account does not exist!");
-        }
+        User foundUser = userRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new BadRequestException("This account does not exist!"));
 
-        User foundUser = userRepository.findByAccountNumber(accountNumber);
         return userMapper.toBankResponse(foundUser);
     }
 
     @Override
     public String nameEnquiry(String accountNumber) {
-        boolean isAccountExist = userRepository.existsByAccountNumber(accountNumber);
-        if (!isAccountExist) {
-            throw new BadRequestException("This account does not exist!");
-        }
+        User foundUser = userRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new BadRequestException("This account does not exist!"));
 
-        User foundUser = userRepository.findByAccountNumber(accountNumber);
         return foundUser.getFirstName() + " " + foundUser.getLastName() + " " + foundUser.getOtherName();
     }
 
     @Override
     public BankResponse creditAccount(CreditDebitRequest creditDebitRequest) {
-        boolean isAccountExist = userRepository.existsByAccountNumber(creditDebitRequest.getAccountNumber());
-        if (!isAccountExist) {
-            throw new BadRequestException("This account does not exist!");
-        }
+        User userToCredit = userRepository.findByAccountNumber(creditDebitRequest.getAccountNumber())
+                .orElseThrow(() -> new BadRequestException("This account does not exist!"));
 
-        User userToCredit = userRepository.findByAccountNumber(creditDebitRequest.getAccountNumber());
         userToCredit.setAccountBalance(
                 userToCredit.getAccountBalance().add(creditDebitRequest.getAmount())
         );
+
         userRepository.save(userToCredit);
 
         return userMapper.toCreditResponse(userToCredit, creditDebitRequest);
@@ -86,18 +77,46 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public BankResponse debitAccount(CreditDebitRequest creditDebitRequest) {
-        boolean isAccountExist = userRepository.existsByAccountNumber(creditDebitRequest.getAccountNumber());
-        if (!isAccountExist) {
-            throw new BadRequestException("This account does not exist!");
-        }
+        User userToDebit = userRepository.findByAccountNumber(creditDebitRequest.getAccountNumber())
+                .orElseThrow(() -> new BadRequestException("This account does not exist!"));
 
-        User userToDebit = userRepository.findByAccountNumber(creditDebitRequest.getAccountNumber());
         if (userToDebit.getAccountBalance().compareTo(creditDebitRequest.getAmount()) < 0) {
             throw new InsufficientBalanceException("Insufficient balance!");
-        } else {
-            userToDebit.setAccountBalance(userToDebit.getAccountBalance().subtract(creditDebitRequest.getAmount()));
-            userRepository.save(userToDebit);
-            return userMapper.toBankResponse(userToDebit);
         }
+
+        userToDebit.setAccountBalance(
+                userToDebit.getAccountBalance().subtract(creditDebitRequest.getAmount())
+        );
+
+        userRepository.save(userToDebit);
+
+        return userMapper.toBankResponse(userToDebit);
+    }
+
+    @Transactional
+    @Override
+    public BankResponse transfer(TransferInfo transferInfo) {
+        User sourceAccountUser = userRepository.findByAccountNumber(transferInfo.getSourceAccountNumber())
+                .orElseThrow(() -> new SourceAccountNotFoundException("Source account does not exist!"));
+
+        User destinationAccountUser = userRepository.findByAccountNumber(transferInfo.getDestinationAccountNumber())
+                .orElseThrow(() -> new DestinationAccountNotFoundException("Destination account does not exist!"));
+
+        if (transferInfo.getAmount().compareTo(sourceAccountUser.getAccountBalance()) > 0) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+
+        sourceAccountUser.setAccountBalance(
+                sourceAccountUser.getAccountBalance().subtract(transferInfo.getAmount())
+        );
+
+        destinationAccountUser.setAccountBalance(
+                destinationAccountUser.getAccountBalance().add(transferInfo.getAmount())
+        );
+
+        userRepository.save(sourceAccountUser);
+        userRepository.save(destinationAccountUser);
+
+        return userMapper.toTransferResponse(sourceAccountUser);
     }
 }
