a345be9afd5d1e71b0129842202605de88ce3443
Berru Hanedar <berruhanedar@icloud.com>
2025-09-09 17:29:54 +0300

    refactor UserRepository to use Optional

--- src/main/java/com/berru/app/bankingapplication/repository/UserRepository.java
+++ src/main/java/com/berru/app/bankingapplication/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.berru.app.bankingapplication.repository;
 import com.berru.app.bankingapplication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
-    Boolean existsByAccountNumber(String accountNumber);
-    User findByAccountNumber(String accountNumber);
+    Optional<User> findByAccountNumber(String accountNumber);
 }
