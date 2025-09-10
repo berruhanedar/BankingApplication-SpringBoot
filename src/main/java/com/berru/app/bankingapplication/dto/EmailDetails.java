aea2042b27346281c5ffdb5bbfd373bf165347d5
Berru Hanedar <berruhanedar@icloud.com>
2025-09-10 03:48:36 +0300

    add validation annotations to request DTOs

--- src/main/java/com/berru/app/bankingapplication/dto/EmailDetails.java
+++ src/main/java/com/berru/app/bankingapplication/dto/EmailDetails.java
@@ -1,5 +1,7 @@
 package com.berru.app.bankingapplication.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,13 +13,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class EmailDetails {
 
+    @NotBlank(message = "Recipient email cannot be blank")
+    @Email(message = "Recipient must be a valid email address")
     private String recipient;
 
+    @NotBlank(message = "Message body cannot be blank")
     private String messageBody;
 
+    @NotBlank(message = "Subject cannot be blank")
     private String subject;
 
     private String attachment;
-
 }
-
