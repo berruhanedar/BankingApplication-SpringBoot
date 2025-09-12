4f9321e4f60a7ae60208c7f550d2bb4768e0082d
Berru Hanedar <berruhanedar@icloud.com>
2025-09-12 05:00:55 +0300

    modify TransactionController for PDF download functionality

--- src/main/java/com/berru/app/bankingapplication/controller/TransactionController.java
+++ src/main/java/com/berru/app/bankingapplication/controller/TransactionController.java
@@ -1,13 +1,13 @@
 package com.berru.app.bankingapplication.controller;
 
-import com.berru.app.bankingapplication.entity.Transaction;
 import com.berru.app.bankingapplication.service.impl.BankStatement;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/bankstatement")
@@ -21,15 +21,14 @@ public class TransactionController {
     }
 
     @GetMapping
-    public List<Transaction> getStatement(
+    public void getStatement(
             @RequestParam String accountNumber,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-    ) {
-        return bankStatement.generateStatement(
-                accountNumber,
-                startDate.toString(),
-                endDate.toString()
-        );
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            HttpServletResponse response
+    ) throws IOException, com.itextpdf.text.DocumentException {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"statement.pdf\"");
+        bankStatement.generateStatement(accountNumber, startDate.toString(), endDate.toString(), response.getOutputStream());
     }
 }
