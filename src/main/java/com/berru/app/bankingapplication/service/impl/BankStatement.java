553455be32dbefdfe77db73c732ea160383405ee
Berru Hanedar <berruhanedar@icloud.com>
2025-09-12 05:00:07 +0300

    refactor BankStatement service and add PDF generation

--- src/main/java/com/berru/app/bankingapplication/service/impl/BankStatement.java
+++ src/main/java/com/berru/app/bankingapplication/service/impl/BankStatement.java
@@ -1,27 +1,48 @@
 package com.berru.app.bankingapplication.service.impl;
 
 import com.berru.app.bankingapplication.entity.Transaction;
+import com.berru.app.bankingapplication.entity.User;
+import com.berru.app.bankingapplication.exception.BadRequestException;
 import com.berru.app.bankingapplication.repository.TransactionRepository;
+import com.berru.app.bankingapplication.repository.UserRepository;
+import com.berru.app.bankingapplication.utils.PdfGenerator;
+import com.itextpdf.text.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.util.List;
+import com.itextpdf.text.DocumentException;
+
 
 @Service
+@Slf4j
 public class BankStatement {
     private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public BankStatement(TransactionRepository transactionRepository) {
+    public BankStatement(TransactionRepository transactionRepository, UserRepository userRepository) {
         this.transactionRepository = transactionRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate) {
-        LocalDateTime start = LocalDate.parse(startDate).atStartOfDay();
-        LocalDateTime end = LocalDate.parse(endDate).atTime(23, 59, 59);
+    public void generateStatement(String accountNumber, String startDate, String endDate, OutputStream outputStream)
+            throws IOException, DocumentException {
+        User user = userRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new BadRequestException("Invalid account number"));
 
-        return transactionRepository.findByAccountNumberAndCreatedAtBetween(accountNumber, start, end);
+        List<Transaction> transactions = transactionRepository.findByAccountNumberAndCreatedAtBetween(
+                accountNumber,
+                LocalDate.parse(startDate).atStartOfDay(),
+                LocalDate.parse(endDate).atTime(23, 59, 59)
+        );
+
+        PdfGenerator.generateBankStatement(transactions, user, startDate, endDate, outputStream);
     }
 }
