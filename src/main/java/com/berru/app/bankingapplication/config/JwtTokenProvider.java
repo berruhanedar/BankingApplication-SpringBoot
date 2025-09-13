ccebc138f7e365c824f082f4ed32aacde72d5686
Berru Hanedar <berruhanedar@icloud.com>
2025-09-13 10:21:08 +0300

    implement JwtTokenProvider for token generation and validation

---
+++ src/main/java/com/berru/app/bankingapplication/config/JwtTokenProvider.java
@@ -0,0 +1,64 @@
+package com.berru.app.bankingapplication.config;
+
+import com.berru.app.bankingapplication.exception.JwtValidationException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException ex) {
+            throw new JwtValidationException("JWT token expired", ex);
+        } catch (MalformedJwtException | SignatureException | IllegalArgumentException ex) {
+            throw new JwtValidationException("Invalid JWT token", ex);
+        }
+    }
+}
