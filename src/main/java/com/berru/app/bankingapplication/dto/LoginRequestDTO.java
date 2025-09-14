e13cea29541db8aeb139c04b123351c638404479
Berru Hanedar <berruhanedar@icloud.com>
2025-09-14 15:49:40 +0300

    create LoginRequestDTO

---
+++ src/main/java/com/berru/app/bankingapplication/dto/LoginRequestDTO.java
@@ -0,0 +1,23 @@
+package com.berru.app.bankingapplication.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 4, max = 100, message = "Password must be at least 4 characters")
+    private String password;
+
+}
