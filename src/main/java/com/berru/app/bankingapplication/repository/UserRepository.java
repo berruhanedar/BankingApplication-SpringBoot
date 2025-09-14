e4ecb8ba34f52b391919f2cf79757a7982eca318
Berru Hanedar <berruhanedar@icloud.com>
2025-09-14 15:46:46 +0300

    add findByEmail method to UserRepository

--- src/main/java/com/berru/app/bankingapplication/repository/UserRepository.java
+++ src/main/java/com/berru/app/bankingapplication/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
     Optional<User> findByAccountNumber(String accountNumber);
+    Optional<User> findByEmail(String email);
 }
